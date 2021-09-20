79eb0ced761896f94aca6e53b35ca968e6dcd21c
Scroomp <dachabot172@gmail.com>
2021-09-19 20:27:20 -0400

    button test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
 function App() {
+  const [numbher, setNumbher] = React.useState(0);
+  const x = [0, 1];
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>ADK Performance</p>
+        <button onClick={() => setNumbher(numbher + 1)}>click me</button>
+        <h2>{numbher}</h2>
       </header>
     </div>
   );
