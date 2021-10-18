ef7ace9583e994e65c6221b18acad32981f3a184
Scroomp <dachabot172@gmail.com>
2021-10-18 13:26:15 -0400

    firebase working

--- src/App.js
+++ src/App.js
@@ -1,67 +1,23 @@
-import "./App.css";
-import React, { useState } from "react";
-import { CSSTransition } from "react-transition-group";
+import React from "react";
+//import "./RecordEntry.css";
+import Firebase from "firebase/compat/app";
+//import 'firebase/auth";
+import "firebase/compat/firestore";
 
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyC1aYleud-AhwrY-_P-mUafEFDmCMmuD1s",
-//   authDomain: "adk-performance.firebaseapp.com",
-//   projectId: "adk-performance",
-//   storageBucket: "adk-performance.appspot.com",
-//   messagingSenderId: "956069877285",
-//   appId: "1:956069877285:web:1475dc822c5f69996d51cc",
-//   measurementId: "G-GC3982DMCZ",
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-
-// var admin = require("firebase-admin");
-
-// var serviceAccount = require("path/to/serviceAccountKey.json");
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-// });
-
-/*
-User
-{
-  role: 'coach' | 'athlete'
-  id: string
-  name: string
-  createdOn: Date
-}
-Exercise
-{
-  id: string
-  name: string
-  unitType: 'kg' | 'seconds' | 'inches'
-  createdOn: Date
-} 
-*/
-// Record;
-// {
-//   id: string;
-//   input: number;
-//   userId: string;
-//   exerciseId: string;
-//   createdOn: Date;
-// }
-
-const user1 = {
-  id: "u-001",
-  role: "athlete",
-  name: "big boi",
+const firebaseConfig = {
+  apiKey: "AIzaSyC1aYleud-AhwrY-_P-mUafEFDmCMmuD1s",
+  authDomain: "adk-performance.firebaseapp.com",
+  projectId: "adk-performance",
+  storageBucket: "adk-performance.appspot.com",
+  messagingSenderId: "956069877285",
+  appId: "1:956069877285:web:1475dc822c5f69996d51cc",
+  measurementId: "G-GC3982DMCZ",
 };
+
+const firebase = Firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
 const clean = {
   id: "c-001",
   name: "clean",
@@ -82,160 +38,55 @@ const Sprint10Yard = {
   name: "10 Yard Sprint",
   unitType: "s",
 };
+const Sprint5105 = {
+  id: "c-005",
+  name: "5 10 5 Sprint",
+  unitType: "s",
+};
 
-const exercises = [snatch, clean, jerk, Sprint10Yard];
-// const cleanRecord = {
-//   id: "cr-001",
-//   userId: "u-001",
-//   exercise: "c-001",
-//   input: 100,
-// };
+const exercises = [Sprint10Yard, Sprint5105, snatch, clean, jerk];
 
-// Athlete -> single data point entry
-// Coach -> multiple data point entry
-//       -> one user for multiple exercise types
-//       -> multiple users for one exercise type
-
-// -- Goals --
-// - [x] Given a list of exercises, render those exercises
-// - [ ] Clicking on an exercise, will open a form, the form will have a number input
-//   - Add a onClick event handler to each button
-//   - That handler will set state of the active exercise (useing React.useState(null) => React.useState("clean "))
-//   - When we have some state, we show a <form>
-//   - {activeExercise !== null ? <form>SOME STUFF</form> : null}
-// - [ ] Submitting the form will update "records" (React state)
-// - [ ] Display the list of records
-
-function App() {
+const RecordEntry = () => {
   const [menuState, setMenuState] = React.useState(exercises[0].id);
-  const [records, setRecords] = React.useState([]);
+  const saveRecord = (event) => {
+    event.preventDefault();
+    const elementsArray = [...event.target.elements];
+    console.log(elementsArray);
+    const formData = elementsArray.reduce((accumulator, currentValue) => {
+      if (currentValue.id) {
+        accumulator[currentValue.id] = currentValue.value;
+      }
+      return accumulator;
+    }, {});
+
+    db.collection("records").add(formData);
+    console.log({ formData });
+  };
 
   return (
-    <>
-      <Navbar>
-        <NavItem icon="I" />
-        <NavItem icon="I" />
-        <NavItem icon="I" />
-        <NavItem icon="O">
-          <DropdownMenu></DropdownMenu>
-        </NavItem>
-      </Navbar>
-      <div>
-        <p>Choose Exercise to Record</p>
-        <form
-          onSubmit={function (event) {
-            event.preventDefault();
-            const formData = new FormData(event.target);
-            console.log();
-            setRecords((records) => [
-              ...records,
-              Object.fromEntries(formData.entries()),
-            ]);
-            console.log(records);
-            event.target.reset();
+    <div className="container">
+      <h1>Enter Record</h1>
+      <form onSubmit={saveRecord}>
+        <select
+          id="exercise"
+          placeholder="Select Exercise"
+          onChange={function (event) {
+            setMenuState(event.target.value);
           }}
         >
-          <select
-            name="exercise"
-            onChange={function (event) {
-              setMenuState(event.target.value);
-            }}
-          >
-            {exercises.map((exercise) => (
-              <option value={exercise.id}>{exercise.name}</option>
-            ))}
-          </select>
-          <input name="record" type="number" />
-          {exercises.find((exercise) => exercise.id === menuState)?.unitType}
-          {""}
-          <button className="submit-button" type="submit">
-            Submit
-          </button>
-        </form>
-      </div>
-      <div>
-        {records
-          .sort((a, b) => +b.record - +a.record)
-          .map((record) => (
-            <button> {record.record}</button>
+          {exercises.map((exercise) => (
+            <option value={exercise.name}>{exercise.name}</option>
           ))}
+        </select>
+        <input type="text" id="name" placeholder="Athlete Name"></input>
+        <input type="number" id="record" placeholder="Record"></input>
+        <button>Submit Record"</button>
+      </form>
+      <div>
+        <h1>Records</h1>
       </div>
-    </>
-  );
-}
-
-function Navbar(props) {
-  return (
-    <nav className="navbar">
-      <ul className="navbar-nav"> {props.children}</ul>
-    </nav>
-  );
-}
-
-function NavItem(props) {
-  const [open, setOpen] = useState(false);
-
-  return (
-    <li className="nav-item">
-      <a href="#" className="itcon-button" onClick={() => setOpen(!open)}>
-        {props.icon}
-      </a>
-      {open && props.children}
-    </li>
-  );
-}
-
-function DropdownMenu() {
-  const [activeMenu, setActiveMenu] = useState("main");
-  function DropdownItem(props) {
-    return (
-      <a
-        href="#"
-        className="menu-item"
-        onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}
-      >
-        <span className="icon-button">{props.leftIcon}</span>
-        {props.children}
-        <span className="icon-button">{props.rightIcon}</span>
-      </a>
-    );
-  }
-  return (
-    <div className="dropdown">
-      <CSSTransition
-        in={activeMenu === "main"}
-        unmountOnExit
-        timeout={500}
-        classNames="menu-primary"
-      >
-        <div className="menu">
-          <DropdownItem>My Profile</DropdownItem>
-          <DropdownItem rightIcon="O" goToMenu="Settings">
-            records
-          </DropdownItem>
-        </div>
-      </CSSTransition>
-
-      <CSSTransition
-        in={activeMenu === "Settings"}
-        unmountOnExit
-        timeout={500}
-        classNames="menu-seconary"
-      >
-        <div className="menu">
-          <DropdownItem leftIcon="O" goToMenu="main" />
-          <DropdownItem>poop</DropdownItem>
-        </div>
-      </CSSTransition>
     </div>
   );
-}
+};
 
-//NAVBAR AND DROPDOWN MENU STUFF
-
-export default App;
-
-// ## Notes
-// - <div name="haha"/> => React.createElement("div", { name: "haha" })
-// - `function () {}`, `() => {}`, `function haha() {}`
-// - Escape to JS with `{}`
+export default RecordEntry;
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,13 +1,5 @@
 import React from "react";
 
 function Form(props) {
-  return (
-    <form>
-      <div>
-        <label>Score</label>
-        <input type="number" />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
-  );
+  return null;
 }
