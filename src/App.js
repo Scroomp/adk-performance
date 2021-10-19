3956e2583272a955f2cd5c22bfbc47ccaf097e27
Scroomp <dachabot172@gmail.com>
2021-10-19 13:52:59 -0400

    basic styling, added new exercises

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import React, { useState, useEffect } from "react";
 import Firebase from "firebase/compat/app";
 //import 'firebase/auth";
 import "firebase/compat/firestore";
+import "./App.css";
+// import Button from "@mui/material/Button";
+// import Select from "@mui/material/Button";
+// import TextField from "@mui/material/Button";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC1aYleud-AhwrY-_P-mUafEFDmCMmuD1s",
@@ -33,18 +37,78 @@ const jerk = {
   name: "jerk",
   unitType: "kg",
 };
-const Sprint10Yard = {
+const Fly10Yard = {
   id: "c-004",
-  name: "10 Yard Sprint",
+  name: "10 Yard Fly",
+  unitType: "s",
+};
+const Fly5Yard = {
+  id: "c-006",
+  name: "5 Yard Fly",
   unitType: "s",
 };
 const Sprint5105 = {
   id: "c-005",
-  name: "5 10 5 Sprint",
+  name: "5-10-5 Sprint",
+  unitType: "s",
+};
+const Fly15Yard = {
+  id: "c-007",
+  name: "15 Yard Fly",
+  unitType: "s",
+};
+const static10 = {
+  id: "c-008",
+  name: "Static 10 Yard Sprint",
+  unitType: "s",
+};
+const static20 = {
+  id: "c-009",
+  name: "Static 20 Yard Sprint",
+  unitType: "s",
+};
+const sprint5515 = {
+  id: "c-010",
+  name: "5-5-15 Sprint",
+  unitType: "s",
+};
+const LDrill = {
+  id: "c-011",
+  name: "L-Drill",
+  unitType: "s",
+};
+const vertical = {
+  id: "c-012",
+  name: "Vertical Jump",
+  unitType: "in",
+};
+const LadderTest = {
+  id: "c-013",
+  name: "Ladder Test",
+  unitType: "s",
+};
+const shuttle150 = {
+  id: "c-014",
+  name: "150yd Shuttle",
   unitType: "s",
 };
 
-const exercises = [Sprint10Yard, Sprint5105, snatch, clean, jerk];
+const exercises = [
+  Fly5Yard,
+  Fly10Yard,
+  Fly15Yard,
+  static10,
+  static20,
+  Sprint5105,
+  sprint5515,
+  LDrill,
+  vertical,
+  LadderTest,
+  shuttle150,
+  snatch,
+  clean,
+  jerk,
+];
 const ref = firebase.firestore().collection("records");
 
 const RecordEntry = () => {
@@ -83,7 +147,7 @@ const RecordEntry = () => {
 
   return (
     <div className="container">
-      <h1>Enter Record</h1>
+      <h1 className="enterRecord">Enter Record</h1>
       <form onSubmit={saveRecord}>
         <select
           id="exercise"
@@ -114,7 +178,7 @@ const RecordEntry = () => {
         <button>Submit Record"</button>
       </form>
       <div>
-        <h1>Records</h1>
+        <h1 className="title">Records</h1>
         <select
           id="exerciseRecord"
           value={menu2State}
@@ -134,12 +198,13 @@ const RecordEntry = () => {
               return record.exercise === menu2State;
             })
             .map((record) => (
-              <li>
+              <li className="recordList">
                 {record.name}, {record.record}
               </li>
             ))}
         </ul>
       </div>
+      <div className="pageBottom"></div>
     </div>
   );
 };
